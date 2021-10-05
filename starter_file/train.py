c150f43a66ccf5f24ae1fbc5db1fcc0d713e728a
dntrply <dntrplytch@gmail.com>
2021-10-04 22:21:35 -0700

    Create train.py

---
+++ starter_file/train.py
@@ -0,0 +1,73 @@
+import argparse
+import os
+import numpy as np
+from sklearn.metrics import mean_squared_error
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+from azureml.core.run import Run
+from azureml.data.dataset_factory import TabularDatasetFactory
+
+from sklearn.linear_model import LogisticRegression
+
+CAPSTONE_TABULAR_WINE_DATA = 'https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/winequality-white.csv'
+
+def clean_data(data):
+
+    # Clean and one hot encode data
+    x_df = data.to_pandas_dataframe().dropna()
+    jobs = pd.get_dummies(x_df.job, prefix="job")
+    x_df.drop("job", inplace=True, axis=1)
+    x_df = x_df.join(jobs)
+    x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
+    x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
+    x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
+    x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
+    contact = pd.get_dummies(x_df.contact, prefix="contact")
+    x_df.drop("contact", inplace=True, axis=1)
+    x_df = x_df.join(contact)
+    education = pd.get_dummies(x_df.education, prefix="education")
+    x_df.drop("education", inplace=True, axis=1)
+    x_df = x_df.join(education)
+    x_df["month"] = x_df.month.map(months)
+    x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
+    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
+
+    y_df = x_df.pop("quality").apply(lambda s: 1 if s > 7 else 0)
+    
+    return x_df, y_df
+
+def main():
+    # Add arguments to script
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+
+    args = parser.parse_args()
+
+    run.log("Regularization Strength:", np.float(args.C))
+    run.log("Max iterations:", np.int(args.max_iter))
+
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+
+    accuracy = model.score(x_test, y_test)
+    run.log("Accuracy", np.float(accuracy))
+
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(model, 'outputs/model.joblib')
+
+# TODO: Create TabularDataset using TabularDatasetFactory
+
+ds = TabularDatasetFactory.from_delimited_files(CAPSTONE_TABULAR_WINE_DATA)
+
+x, y = clean_data(ds)
+
+# Split data into train and test sets.
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+run = Run.get_context()
+
+if __name__ == '__main__':
+    main()
